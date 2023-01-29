092fe20bb45b27e6172bcf0d1d6982b32781c7a8
Taeyoung Kim <lucy_ia@ewhain.net>
2023-01-29 22:25:51 +0900

    signup, login 기능 추가

--- src/main/java/com/example/evision/service/UserService.java
+++ src/main/java/com/example/evision/service/UserService.java
@@ -29,13 +29,9 @@ public class UserService {
         Users user = usersRepository.findByUserId(loginDTO.getUserId());
 
         // 아이디가 존재하지 않는경우 -2(로그인 실패-아이디 조회 불가) 반환
-        if(!usersRepository.existsByUserId(loginDTO.getUserId())){
-            return -2;
-        }
+        if(!usersRepository.existsByUserId(loginDTO.getUserId())){ return -2; }
         // 비밀번호 일치하지 않는 경우 -1(로그인 실패) 반환
-        if(!Objects.equals(user.getUserPw(), loginDTO.getUserPw())){
-            return -1;
-        }
+        if(!Objects.equals(user.getUserPw(), loginDTO.getUserPw())){ return -1; }
 
         return user.getAuthStatus();
     }
